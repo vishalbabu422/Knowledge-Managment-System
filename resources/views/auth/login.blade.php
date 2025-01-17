f33d2da29403a88cb24bbe529050cc79bd64a61f
Vishal Babu <vishalbabu422@gmail.com>
2025-01-17 17:17:53 +0530

    login changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,15 +13,19 @@
                     <div class="login-wrap p-0">
                         <h3 class="mb-4 text-center">Have an account?</h3>
                         <form method="POST" action="{{ route('login') }}" class="signin-form">
+                            @csrf
                             <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Username" required>
+                                <input type="text" class="form-control" placeholder="Email" id="email" :value="old('email')" name="email" required>
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
                             <div class="form-group">
-                                <input id="password-field" type="password" class="form-control" placeholder="Password" required>
+                                <input id="password" name="password" type="password" class="form-control" placeholder="Password" required>
                                 <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
                             </div>
+
                             <div class="form-group">
-                                <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+                                <button type="submit" class="form-control btn btn-primary submit px-3">{{ __('Log in') }}</button>
                             </div>
                             <div class="form-group d-md-flex">
                                 <div class="w-50">
@@ -35,49 +39,10 @@
                                 </div>
                             </div>
                         </form>
-                        
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!--form method="POST" action="{{ route('login') }}">
-        @csrf
 
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                type="password"
-                name="password"
-                required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
-            </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form -->
-    </x-guest-layout>
+</x-login-layout>
