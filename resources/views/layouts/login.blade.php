e6d852a62fc194f7a613783eff9623b1b9d940d0
Vishal Babu <vishalbabu422@gmail.com>
2025-01-18 14:08:37 +0530

    login layout changed

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -19,18 +19,8 @@
     @vite(['resources/css/style.css'])
 </head>
 
-<body class="img js-fullheight" style="background-image: url({{ Vite::asset('resources/images/bg.jpg')}});">
-    <!-- <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </div>
-
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            {{ $slot }}
-        </div>
-    </div-->
+<body class="img js-fullheight" style="background-image: url({{ Vite::asset('resources/images/bg2.jpg')}});">
+    {{ $slot }}
 </body>
 
 </html>
